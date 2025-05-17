7e4c5624b2dd2fc2d0d183b00ffad627701c902a
jon65 <xinquanyip@gmail.com>
2025-05-18 01:58:27 +1000

    add

---
+++ quickBook/Models/Appointment.cs
@@ -0,0 +1,17 @@
+// Models/Appointment.cs
+public class Appointment
+{
+    public string Id { get; set; }
+    public string UserId { get; set; }
+    public DateTime Date { get; set; }
+    public string Time { get; set; }
+    public string Status { get; set; }
+}
+
+// Models/AppointmentRequest.cs
+public class AppointmentRequest
+{
+    public string UserId { get; set; }
+    public DateTime Date { get; set; }
+    public string Time { get; set; }
+}
